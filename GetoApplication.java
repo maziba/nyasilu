a159bd1fb8f4bd733e19fcc5a01673f45c5b722e
maziba <132705601+maziba@users.noreply.github.com>
2023-06-08 15:45:31 +0300

    Add files via upload

---
+++ GetoApplication.java
@@ -0,0 +1,13 @@
+package com.example.geto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetoApplication.class, args);
+	}
+
+}
